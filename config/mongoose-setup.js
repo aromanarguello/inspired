9ce5a0e7a9df941bd0a16cde41ae92e2a4613029
Alejandro Roman <aromanarguello@gmail.com>
2017-11-29 00:33:01 -0500

    first commit, express/node/mongoose setup

---
+++ config/mongoose-setup.js
@@ -0,0 +1,12 @@
+const mongoose = require( "mongoose" );
+
+mongoose.Promise = Promise;
+
+mongoose.connect( 'mongodb://localhost/inspired', { useMongoClient: true } )
+.then( () => {
+  console.log( 'Mongoose Connected!!!✨✨✨✨✨✨✨✨' );
+})
+.catch( err => {
+  console.log( 'Mongoose connection FAILED!!! 🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨' );
+  console.log( err );
+})
