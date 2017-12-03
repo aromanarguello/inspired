50d78615be3b7a560a39d74d5f1ddb4b8749fae0
Alejandro Roman <aromanarguello@gmail.com>
2017-12-03 17:21:51 -0500

    ads

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const layouts      = require('express-ejs-layouts');
 
-require("dotenv").config()
+require("dotenv").config();
 
 require("./config/mongoose-setup.js");
 
