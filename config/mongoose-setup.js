97254dd3d04637e250f0bb5052e6da44330beaac
Alejandro Roman <aromanarguello@gmail.com>
2017-12-03 18:03:23 -0500

    changed uri

--- config/mongoose-setup.js
+++ config/mongoose-setup.js
@@ -2,7 +2,7 @@ const mongoose = require( "mongoose" );
 
 mongoose.Promise = Promise;
 
-mongoose.connect( "mongodb://localhost/inspired", { useMongoClient: true } )
+mongoose.connect( process.env.MONGODB_URI, { useMongoClient: true } )
 .then( () => {
   console.log( 'Mongoose Connected!!!✨✨✨✨✨✨✨✨' );
 })
