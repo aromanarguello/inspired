1a4e50d8bbfd98a6cb406dff9c316e6da9846d62
Alejandro Roman <aromanarguello@gmail.com>
2017-12-04 08:59:39 -0500

    credentials

--- config/mongoose-setup.js
+++ config/mongoose-setup.js
@@ -2,7 +2,7 @@ const mongoose = require( "mongoose" );
 
 mongoose.Promise = Promise;
 
-mongoose.connect( "mongodb://heroku_vrvkst0h:tvh2foc8p0imto5bt0i5cfpo7a@ds129796.mlab.com:29796/heroku_vrvkst0h", { useMongoClient: true } )
+mongoose.connect( process.env.MONGODB_URI, { useMongoClient: true } )
 .then( () => {
   console.log( 'Mongoose Connected!!!✨✨✨✨✨✨✨✨' );
 })
